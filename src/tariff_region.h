f561a2bc15d3939fb28ddc807b9a39574da3dec2
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-03 18:13:46 -0500

    initial commit for a tariff region, for some reason runs are hanging

---
+++ src/tariff_region.h
@@ -0,0 +1,34 @@
+#ifndef CYCLUS_AGENTS_TARIFF_REGION_H_
+#define CYCLUS_AGENTS_TARIFF_REGION_H_
+
+#include <string>
+
+#include "cyclus.h"
+
+namespace cyclus {
+
+class TariffRegion : public cyclus::Region {
+ public:
+  TariffRegion(cyclus::Context* ctx);
+  virtual ~TariffRegion();
+
+  #pragma cyclus
+
+  #pragma cyclus note {"doc": "A region that applies tarrifs to trades with " \
+                              "other regions."}
+
+  /// adjust preferences for materials based on user input for tariffs
+  virtual double AdjustMatlPref(Request<Material>* req, Bid<Material>* bid,
+                                double pref, TradeSense sense);
+
+
+ protected:
+  // todo: add docs, etc.
+  // map of region_name: time: value
+  #pragma cyclus var {}
+  std::map<int, std::map<std::string, double> > tariff_vals;
+};
+
+}  // namespace cyclus
+
+#endif  // CYCLUS_AGENTS_TARIFF_REGION_H_
