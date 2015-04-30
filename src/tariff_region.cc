244141b398065de17865920ecd122e642825fdf9
Matthew Gidden <matthew.gidden@gmail.com>
2015-04-30 13:31:44 -0500

    naming conventions to support future tariff types

--- src/tariff_region.cc
+++ src/tariff_region.cc
@@ -10,25 +10,27 @@ TariffRegion::~TariffRegion() {}
 
 void TariffRegion::Tick() {
   int time = context()->time();
-  if (tariff_vals.find(time) != tariff_vals.end()) {
-    std::map<std::string, double>& vals = tariff_vals[time];
+  if (region_tariffs.find(time) != region_tariffs.end()) {
+    std::map<std::string, double>& vals = region_tariffs[time];
     std::map<std::string, double>::iterator it;
     for (it = vals.begin(); it != vals.end(); ++it) {
-      current_tariffs_[it->first] = it->second;
+      current_region_tariffs_[it->first] = it->second;
     }
   }
 }
 
 double TariffRegion::AdjustMatlPref(Request<Material>* req, Bid<Material>* bid,
                                     double pref, TradeSense sense) {
+  // region tariff
   Agent* other = sense == BID ?
                  req->requester()->manager() : bid->bidder()->manager();
   other = other->parent()->parent(); // region is parent of parent
   std::string name = other->prototype();
-  if (current_tariffs_.find(name) != current_tariffs_.end()) {
-    LOG(cyclus::LEV_INFO4, "TariffR") << current_tariffs_[name];
-    pref *= current_tariffs_[name];
+  if (current_region_tariffs_.find(name) != current_region_tariffs_.end()) {
+    LOG(cyclus::LEV_INFO4, "TariffR") << current_region_tariffs_[name];
+    pref *= current_region_tariffs_[name];
   }
+  
   LOG(cyclus::LEV_INFO4, "TariffR") << "Final pref: " << pref;
   return pref;
 }
